25bab565cb0e91d1f3f6b9baca925f02751c07aa
Landon Orr <landonwo@gmail.com>
2017-07-26 16:48:37 -0600

    Basic logic done, need helpful text though.

--- bot.py
+++ bot.py
@@ -1,33 +1,52 @@
 import praw
 import credentials
 
-keywords = ['help', 'newbie', 'noob', 'dying', 'trouble','new', 'advice', 'advise', 'tips', ' id ', 'id.', 'id?',
-            'identify', 'save', 'trouble']
-possiblekeywords = ['vft', 'flytrap', 'fly trap']
 
-reddit = praw.Reddit(client_id=credentials.id,
-                     client_secret=credentials.secret,
-                     password=credentials.password,
-                     user_agent='SavageGarden Moderator by /u/Metroshica',
-                     username=credentials.username)
+# Initial connection and subreddit selection
+def connect():
+    reddit = praw.Reddit(client_id=credentials.id,
+                         client_secret=credentials.secret,
+                         password=credentials.password,
+                         user_agent='SavageGarden Moderator by /u/Metroshica',
+                         username=credentials.username)
 
-subreddit = reddit.subreddit('savagegarden')
+    return reddit.subreddit('savagegarden')
 
-count = 0
-for submission in subreddit.new(limit=100):
-    #if any(word in submission.title.lower() for word in keywords):
-    printstring = submission.title
-    found = False
-    #for word in keywords:
-        #if word in submission.title.lower():
-         #   printstring = printstring + " " + word
-        #    found = True
-    #if found == True:
-        #print(printstring)
-    print(submission.title)
-    #count = count + 1
 
-print(count)
+# Find posts that have one or more new grower keywords
+def findposts(subreddit):
+    keywords = [
+        'help', 'newbie', 'noob', 'dying', 'trouble', 'new', 'advice', 'advise', 'tips', ' id ', 'id.', 'id?',
+        'identify', 'save', 'trouble'
+    ]
+    for submission in subreddit.new(limit=20):
+        if any(word in submission.title.lower() for word in keywords):
+            if checkifresponded(submission) is False:
+                respond(submission)
 
-def respond():
+
+# Check if bot has already responded or not
+def checkifresponded(submission):
+    responded = False
+    commentlist = submission.comments.list()
+    for comment in commentlist:
+        if comment.author == 'SavageGardenBot':
+            responded = True
+    return responded
+
+
+# Respond to post with helpful links
+def respond(submission):
+    responsetext = "Looks like you're new here. Here's some info..."
+    submission.reply(responsetext)
+    print('Found one')
     pass
+
+
+def main():
+    subreddit = connect()
+    findposts(subreddit)
+
+
+if __name__ == "__main__":
+    main()
